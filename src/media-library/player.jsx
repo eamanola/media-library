ce4c935e5b67ec17321bd857c62624775e5aefdb
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2024-07-25 19:28:04 +0300

    wait till play can start

--- src/media-library/player.jsx
+++ src/media-library/player.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
@@ -9,7 +9,15 @@ const Player = () => {
   const { videoId } = useParams();
   const [transcodeVideo, setTranscodeVideo] = useState(false);
   const [transcodeAudio, setTranscodeAudio] = useState(false);
+  const [canPlay, setCanPlay] = useState([]);
   const audioRef = useRef(null);
+
+  useEffect(() => {
+    console.log(canPlay);
+    if (canPlay.length === 2) {
+      console.log('Can start');
+    }
+  }, [canPlay]);
 
   if (!mediaLibrary.length) return null;
 
@@ -60,14 +68,27 @@ const Player = () => {
     }
   };
 
+  const onCanPlay = ({ target }) => {
+    setCanPlay((state) => [...state.filter((el) => el !== target.tagName), target.tagName]);
+  };
   const onPlay = () => audioRef.current.play();
   const onPause = () => audioRef.current.pause();
   const onSeeked = ({ target }) => { audioRef.current.currentTime = target.currentTime; };
 
+  const canStart = () => canPlay.length === 2;
   return (
     <>
+      { !canStart() && (<div>Loading...</div>) }
       <video
+        style={{
+          visibility: canStart() ? 'visible' : 'hidden',
+          maxHeight: '90vh',
+          maxWidth: '100vw',
+          margin: '0 auto',
+          display: 'block',
+        }}
         controls
+        onCanPlay={onCanPlay}
         onError={onError}
         onPlay={onPlay}
         onPause={onPause}
@@ -76,7 +97,7 @@ const Player = () => {
         <track kind="captions" label="foo" />
         <source src={videoSrc()} type="video/mp4" />
       </video>
-      <audio ref={audioRef}>
+      <audio ref={audioRef} onCanPlay={onCanPlay}>
         <track kind="captions" label="foo" />
         <source src={audioSrc()} type="video/mp4" />
       </audio>
